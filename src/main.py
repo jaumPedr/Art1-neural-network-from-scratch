985198620df4a23d64c8ae5824aea7446609a51e
jaumPedr <jpma306@gmail.com>
2026-06-13 12:34:21 -0300

    model validation  implementation

--- src/main.py
+++ src/main.py
@@ -3,23 +3,33 @@ import data_module as data
 import numpy as np
 import pandas as pd
 
+
 #ART Test  
 X, y = data.get_data()
-X_train, y_train, X_test, y_test, X_validation, y_validation = data.train_test_validation_split(X,y)
+X = data.quartiles_dummy_variables(X, q=2)
 
-X = data.quartiles_dummy_variables(X)
+X_train, y_train, X_validation, y_validation, X_test, y_test = data.train_test_validation_split(X,y)
 
-model = art.Art(X.shape[1], 1, p = 0.5)
-Y, K = art.train_loop(model, X)
+model = art.Art(X_train.shape[1], 1, p = 0.75)
+Y, K = art.train_loop(model, X_train)
 
 np.savetxt('./results/art_output_classes.csv', K, delimiter=',', fmt='%d')
 np.savetxt('./results/art_output_one_hot_encoding.csv', Y, delimiter=',', fmt='%d')
 print('Number of Classes: ', model.recognition_layer_size)
-
+print('Number of Features: ',X[1].size)
 print(y.squeeze)
 
 df = pd.DataFrame({
-    'Classe_Real': y.squeeze(),
+    'Classe_Real': y_train.squeeze(),
+    'Categoria_ART': K
+})
+
+print(pd.crosstab(df['Categoria_ART'], df['Classe_Real']))
+
+Y, K = art.predict(model, X_validation)
+
+df = pd.DataFrame({
+    'Classe_Real': y_validation.squeeze(),
     'Categoria_ART': K
 })
 
