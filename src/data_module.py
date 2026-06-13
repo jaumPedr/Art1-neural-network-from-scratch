736c5412f93c9a1c7add45758ee7cd92e66179dd
jaumPedr <jpma306@gmail.com>
2026-06-13 12:32:18 -0300

    quartiles separation update

--- src/data_module.py
+++ src/data_module.py
@@ -14,22 +14,21 @@ def get_data():
     #features and targets
     features = dry_bean.data.features 
     targets = dry_bean.data.targets
-    
     return features, targets
 
 def train_test_validation_split(features, targets):
     X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=TEST_SPLIT, random_state=RANDOM_SEED)
-    X_train, X_validation, y_train, y_validation = train_test_split(features, targets, test_size=VALIDATION_SPLIT, random_state=RANDOM_SEED)
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=VALIDATION_SPLIT, random_state=RANDOM_SEED)
 
-    return X_train, y_train, X_test, y_test, X_validation, y_validation
+    return X_train, y_train, X_validation, y_validation, X_test, y_test
 
-def quartiles_dummy_variables(features):
+def quartiles_dummy_variables(features, q=4):
     
     X_quartiles = []
 
     #Separete each colum in 4 quartiles
     for col in features.columns:
-        bins = pd.qcut(features[col], q=4, duplicates='drop')
+        bins = pd.qcut(features[col], q=q, duplicates='drop')
         X_quartiles.append(pd.get_dummies(bins, prefix=col))
 
     #one-hot encoding, each quartiles is a feature (Dummy Variables)
